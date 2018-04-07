219b15ecbe559f531e9612d684dc93c8ca0324ba
Tom Crockett <pelotom@gmail.com>
2018-04-07 13:36:34 -0700

    Format all the things

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,210 +1,242 @@
-import { ofType, unionize } from '.'
+import { ofType, unionize } from '.';
 
 describe('merged', () => {
-
   const Foo = unionize({
     x: ofType<{ n: number }>(),
     y: ofType<{ s: string }>(),
-  })
+  });
 
-  let foo: typeof Foo._Union
+  let foo: typeof Foo._Union;
 
   beforeEach(() => {
-    foo = Foo.x({ n: 3 })
-  })
+    foo = Foo.x({ n: 3 });
+  });
 
   it('creation', () => {
     expect(foo).toEqual({
       tag: 'x',
       n: 3,
-    })
-  })
+    });
+  });
 
   it('predicates', () => {
-    expect(Foo.is.x(foo)).toBe(true)
-    expect(Foo.is.y(foo)).toBe(false)
-  })
+    expect(Foo.is.x(foo)).toBe(true);
+    expect(Foo.is.y(foo)).toBe(false);
+  });
 
   it('casts', () => {
-    expect(Foo.as.x(foo).n).toBe(3)
-    expect(() => Foo.as.y(foo)).toThrow('Attempted to cast x as y')
-  })
+    expect(Foo.as.x(foo).n).toBe(3);
+    expect(() => Foo.as.y(foo)).toThrow('Attempted to cast x as y');
+  });
 
   it('matching', () => {
-    expect(Foo.match({
-      x: ({ n }) => n + 9,
-      y: ({ s }) => s.length,
-    })(foo)).toBe(12)
-    expect(Foo.match({
-      y: ({ s }) => s.length,
-      default: _ => 42
-    })(foo)).toBe(42)
-  })
-  
+    expect(
+      Foo.match({
+        x: ({ n }) => n + 9,
+        y: ({ s }) => s.length,
+      })(foo),
+    ).toBe(12);
+    expect(
+      Foo.match({
+        y: ({ s }) => s.length,
+        default: _ => 42,
+      })(foo),
+    ).toBe(42);
+  });
+
   it('accepts undefined props with default for matching', () => {
-    expect(Foo.match({
-      y: ({ s }) => s.length,
-      x: undefined,
-      default: () => 42,
-    })(Foo.x({n:3}))).toBe(42)
-  })
+    expect(
+      Foo.match({
+        y: ({ s }) => s.length,
+        x: undefined,
+        default: () => 42,
+      })(Foo.x({ n: 3 })),
+    ).toBe(42);
+  });
 
   it('default accepts initial object for matching', () => {
-    expect(Foo.match({ default: f => f })(foo)).toBe(foo)
-  })
+    expect(Foo.match({ default: f => f })(foo)).toBe(foo);
+  });
 
   describe('name conflicts', () => {
     it('avoidable', () => {
-      const T = unionize({
-        foo: ofType<{ x: number }>(),
-      }, 'conflict')
-      const input = { x: 42, conflict: 'oops' }
-      expect(T.foo(input).conflict).toBe('foo')
-    })
-  })
-})
+      const T = unionize(
+        {
+          foo: ofType<{ x: number }>(),
+        },
+        'conflict',
+      );
+      const input = { x: 42, conflict: 'oops' };
+      expect(T.foo(input).conflict).toBe('foo');
+    });
+  });
+});
 
 describe('separate', () => {
-
-  const Foo = unionize({
-    x: ofType<number>(),
-    y: ofType<string>(),
-  }, 'flim', 'flam')
-
-  let foo: typeof Foo._Union
-
-  beforeEach(() => {
-    foo = Foo.x(3)
-  })
-
-  it('creation', () => {
-    expect(foo.flim).toBe('x')
-    expect(foo.flam).toBe(3)
-  })
-
-  it('predicates', () => {
-    expect(Foo.is.x(foo)).toBe(true)
-    expect(Foo.is.y(foo)).toBe(false)
-  })
-
-  it('casts', () => {
-    expect(Foo.as.x(foo)).toBe(3)
-    expect(() => Foo.as.y(foo)).toThrow('Attempted to cast x as y')
-  })
-
-  it('matching', () => {
-    expect(Foo.match({
-      x: n => n + 9,
-      y: s => s.length,
-    })(foo)).toBe(12)
-
-    expect(Foo.match({
-      y: s => s.length,
-      default: _ => 42,
-    })(foo)).toBe(42)
-  })
-
-  it('accepts undefined props with default for matching', () => {
-    expect(Foo.match({
-      y: s => s.length,
-      x: undefined,
-      default: () => 42,
-    })(Foo.x(3))).toBe(42)
-  })
-
-  it('default accepts initial object for matching', () => {
-    expect(Foo.match({ default: f => f })(foo)).toBe(foo)
-  })
-
-  it('enumerable tags', () => {
-    const Bar = unionize({
+  const Foo = unionize(
+    {
       x: ofType<number>(),
       y: ofType<string>(),
-      z: ofType<boolean>(),
-    }, 'blum', 'blam')
+    },
+    'flim',
+    'flam',
+  );
 
-    const bar = Foo.match(Bar)(foo)
-    expect(bar.blum).toBe('x')
-    expect(bar.blam).toBe(3)
-  })
-})
-
-describe('spreads', () => {
-
-  const Parents = unionize({
-    fred: ofType<string>(),
-    wilma: ofType<string>(),
-  }, 'name', 'catchphrase')
-
-  const Flintstones = unionize({
-    pebbles: ofType<string>(),
-    ...Parents._Record
-  }, 'name', 'catchphrase')
-
-  let fred: typeof Flintstones._Union
-  let wilma: typeof Flintstones._Union
-  let pebbles: typeof Flintstones._Union
+  let foo: typeof Foo._Union;
 
   beforeEach(() => {
-    fred = Flintstones.fred('Yabba-Dabba-Doo!')
-    wilma = Flintstones.wilma('Da-da-da duh da-da CHARGE IT!!')
-    pebbles = Flintstones.pebbles('Yabba-Dabba-Doozie!')
-  })
+    foo = Foo.x(3);
+  });
+
+  it('creation', () => {
+    expect(foo.flim).toBe('x');
+    expect(foo.flam).toBe(3);
+  });
+
+  it('predicates', () => {
+    expect(Foo.is.x(foo)).toBe(true);
+    expect(Foo.is.y(foo)).toBe(false);
+  });
+
+  it('casts', () => {
+    expect(Foo.as.x(foo)).toBe(3);
+    expect(() => Foo.as.y(foo)).toThrow('Attempted to cast x as y');
+  });
+
+  it('matching', () => {
+    expect(
+      Foo.match({
+        x: n => n + 9,
+        y: s => s.length,
+      })(foo),
+    ).toBe(12);
+
+    expect(
+      Foo.match({
+        y: s => s.length,
+        default: _ => 42,
+      })(foo),
+    ).toBe(42);
+  });
+
+  it('accepts undefined props with default for matching', () => {
+    expect(
+      Foo.match({
+        y: s => s.length,
+        x: undefined,
+        default: () => 42,
+      })(Foo.x(3)),
+    ).toBe(42);
+  });
+
+  it('default accepts initial object for matching', () => {
+    expect(Foo.match({ default: f => f })(foo)).toBe(foo);
+  });
+
+  it('enumerable tags', () => {
+    const Bar = unionize(
+      {
+        x: ofType<number>(),
+        y: ofType<string>(),
+        z: ofType<boolean>(),
+      },
+      'blum',
+      'blam',
+    );
+
+    const bar = Foo.match(Bar)(foo);
+    expect(bar.blum).toBe('x');
+    expect(bar.blam).toBe(3);
+  });
+});
+
+describe('spreads', () => {
+  const Parents = unionize(
+    {
+      fred: ofType<string>(),
+      wilma: ofType<string>(),
+    },
+    'name',
+    'catchphrase',
+  );
+
+  const Flintstones = unionize(
+    {
+      pebbles: ofType<string>(),
+      ...Parents._Record,
+    },
+    'name',
+    'catchphrase',
+  );
+
+  let fred: typeof Flintstones._Union;
+  let wilma: typeof Flintstones._Union;
+  let pebbles: typeof Flintstones._Union;
+
+  beforeEach(() => {
+    fred = Flintstones.fred('Yabba-Dabba-Doo!');
+    wilma = Flintstones.wilma('Da-da-da duh da-da CHARGE IT!!');
+    pebbles = Flintstones.pebbles('Yabba-Dabba-Doozie!');
+  });
 
   it('creator', () => {
     expect(pebbles).toEqual({
       name: 'pebbles',
-      catchphrase: 'Yabba-Dabba-Doozie!'
-    })
+      catchphrase: 'Yabba-Dabba-Doozie!',
+    });
     expect(fred).toEqual({
       name: 'fred',
-      catchphrase: 'Yabba-Dabba-Doo!'
-    })
+      catchphrase: 'Yabba-Dabba-Doo!',
+    });
     expect(Flintstones.wilma('Da-da-da duh da-da CHARGE IT!!')).toEqual({
       name: 'wilma',
-      catchphrase: 'Da-da-da duh da-da CHARGE IT!!'
-    })
-  })
+      catchphrase: 'Da-da-da duh da-da CHARGE IT!!',
+    });
+  });
 
   it('predicate', () => {
-    expect(Flintstones.is.pebbles(pebbles)).toEqual(true)
-    expect(Flintstones.is.pebbles(pebbles)).toEqual(true)
-  })
+    expect(Flintstones.is.pebbles(pebbles)).toEqual(true);
+    expect(Flintstones.is.pebbles(pebbles)).toEqual(true);
+  });
 
   it('cast', () => {
-    expect(Flintstones.as.pebbles(pebbles)).toBe('Yabba-Dabba-Doozie!')
-    expect(() => Flintstones.as.fred(pebbles)).toThrow('Attempted to cast pebbles as fred')
-    expect(Flintstones.as.fred(fred)).toBe('Yabba-Dabba-Doo!')
-    expect(() => Flintstones.as.wilma(fred)).toThrow('Attempted to cast fred as wilma')
-    expect(() => Flintstones.as.pebbles(fred)).toThrow('Attempted to cast fred as pebbles')
-  })
+    expect(Flintstones.as.pebbles(pebbles)).toBe('Yabba-Dabba-Doozie!');
+    expect(() => Flintstones.as.fred(pebbles)).toThrow('Attempted to cast pebbles as fred');
+    expect(Flintstones.as.fred(fred)).toBe('Yabba-Dabba-Doo!');
+    expect(() => Flintstones.as.wilma(fred)).toThrow('Attempted to cast fred as wilma');
+    expect(() => Flintstones.as.pebbles(fred)).toThrow('Attempted to cast fred as pebbles');
+  });
 
   it('matching', () => {
-    expect(Flintstones.match({
-      fred: c => 'Father says: ' + c,
-      wilma: c => c,
-      pebbles: c => c
-    })(fred)).toBe('Father says: Yabba-Dabba-Doo!')
-    expect(Flintstones.match({
-      fred: c => c,
-      wilma: c => c,
-      pebbles: c => 'Daughter says: ' + c
-    })(pebbles)).toBe('Daughter says: Yabba-Dabba-Doozie!')
-  })
+    expect(
+      Flintstones.match({
+        fred: c => 'Father says: ' + c,
+        wilma: c => c,
+        pebbles: c => c,
+      })(fred),
+    ).toBe('Father says: Yabba-Dabba-Doo!');
+    expect(
+      Flintstones.match({
+        fred: c => c,
+        wilma: c => c,
+        pebbles: c => 'Daughter says: ' + c,
+      })(pebbles),
+    ).toBe('Daughter says: Yabba-Dabba-Doozie!');
+  });
 
   // note: TagProp and ValProp of spreaded _Record has no signficance on final record
   it('creation with different TagProp and ValProp', () => {
-    const foo = unionize({ a: ofType<{ x: number}>() }, 'type', 'payload')
-    const bar = unionize({ b: ofType<{ y: string}>(), ...foo._Record }, 'schmype', 'schmayload')
+    const foo = unionize({ a: ofType<{ x: number }>() }, 'type', 'payload');
+    const bar = unionize({ b: ofType<{ y: string }>(), ...foo._Record }, 'schmype', 'schmayload');
 
     expect(bar.a({ x: 1 })).toEqual({
       schmype: 'a',
-      schmayload: { x: 1 }
-    })
+      schmayload: { x: 1 },
+    });
 
     expect(bar.b({ y: 'qwertz' })).toEqual({
       schmype: 'b',
-      schmayload: { y: 'qwertz' }
-    })
-  })
-})
+      schmayload: { y: 'qwertz' },
+    });
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,52 @@
 export type Unionized<Record, TaggedRecord> = {
   _Tags: keyof TaggedRecord;
   _Record: Record;
-  _Union: TaggedRecord[keyof TaggedRecord]
-  is: Predicates<TaggedRecord>
-  as: Casts<Record, TaggedRecord[keyof TaggedRecord]>
-  match: Match<Record, TaggedRecord[keyof TaggedRecord]>
-} & Creators<Record, TaggedRecord>
+  _Union: TaggedRecord[keyof TaggedRecord];
+  is: Predicates<TaggedRecord>;
+  as: Casts<Record, TaggedRecord[keyof TaggedRecord]>;
+  match: Match<Record, TaggedRecord[keyof TaggedRecord]>;
+} & Creators<Record, TaggedRecord>;
 
 export type Creators<Record, TaggedRecord> = {
   [T in keyof Record]: (value: Record[T]) => TaggedRecord[keyof TaggedRecord]
-}
+};
 
 export type Predicates<TaggedRecord> = {
-  [T in keyof TaggedRecord]: (variant: TaggedRecord[keyof TaggedRecord]) => variant is TaggedRecord[T]
-}
+  [T in keyof TaggedRecord]: (
+    variant: TaggedRecord[keyof TaggedRecord],
+  ) => variant is TaggedRecord[T]
+};
 
-export type Casts<Record, Union> = {
-  [T in keyof Record]: (variant: Union) => Record[T]
-}
+export type Casts<Record, Union> = { [T in keyof Record]: (variant: Union) => Record[T] };
 
-export type Cases<Record, A> = {
-  [T in keyof Record]: (value: Record[T]) => A
-}
+export type Cases<Record, A> = { [T in keyof Record]: (value: Record[T]) => A };
 
 export type Match<Record, Union> = {
   <A>(
     cases:
       | Cases<Record, A> & NoDefaultProp
-      | Partial<Cases<Record, A>> & { default: (variant: Union) => A }
-  ): (variant: Union) => A
-}
+      | Partial<Cases<Record, A>> & { default: (variant: Union) => A },
+  ): (variant: Union) => A;
+};
 
 export type MultiValueVariants<Record extends MultiValueRec, TagProp extends string> = {
   [T in keyof Record]: { [_ in TagProp]: T } & Record[T]
-}
+};
 
-export type SingleValueVariants<Record extends SingleValueRec, TagProp extends string, ValProp extends string> = {
-  [T in keyof Record]: { [_ in TagProp]: T } & { [_ in ValProp]: Record[T] }
-}
+export type SingleValueVariants<
+  Record extends SingleValueRec,
+  TagProp extends string,
+  ValProp extends string
+> = { [T in keyof Record]: { [_ in TagProp]: T } & { [_ in ValProp]: Record[T] } };
 
 // Forbid usage of default property; reserved for pattern matching.
-export type NoDefaultProp = { default?: never }
+export type NoDefaultProp = { default?: never };
 
-export type SingleValueRec = NoDefaultRec<{} | null>
-export type MultiValueRec = NoDefaultRec<{ [tag: string]: any }>
+export type SingleValueRec = NoDefaultRec<{} | null>;
+export type MultiValueRec = NoDefaultRec<{ [tag: string]: any }>;
 export type NoDefaultRec<Val> = {
-  [k: string]: Val
-} & NoDefaultProp
+  [k: string]: Val;
+} & NoDefaultProp;
 
 /**
  * Create a tagged union from a record mapping tags to value types, along with associated
@@ -59,59 +59,62 @@ export type NoDefaultRec<Val> = {
  * the value must be a dictionary type.
  */
 export function unionize<Record extends MultiValueRec>(
-  record: Record
-): Unionized<Record, MultiValueVariants<Record, 'tag'>>
+  record: Record,
+): Unionized<Record, MultiValueVariants<Record, 'tag'>>;
 export function unionize<Record extends MultiValueRec, TagProp extends string>(
   record: Record,
   tagProp: TagProp,
-): Unionized<Record, MultiValueVariants<Record, TagProp>>
-export function unionize<Record extends SingleValueRec, TagProp extends string, ValProp extends string>(
+): Unionized<Record, MultiValueVariants<Record, TagProp>>;
+export function unionize<
+  Record extends SingleValueRec,
+  TagProp extends string,
+  ValProp extends string
+>(
   record: Record,
   tagProp: TagProp,
   valProp: ValProp,
-): Unionized<Record, SingleValueVariants<Record, TagProp, ValProp>>
+): Unionized<Record, SingleValueVariants<Record, TagProp, ValProp>>;
 export function unionize<Record>(record: Record, tagProp = 'tag', valProp?: string) {
-  const creators = {} as Creators<Record, any>
+  const creators = {} as Creators<Record, any>;
   for (const tag in record) {
     creators[tag] = (value: any) =>
-      valProp
-        ? { [tagProp]: tag, [valProp]: value }
-        : { ...value, [tagProp]: tag }
+      valProp ? { [tagProp]: tag, [valProp]: value } : { ...value, [tagProp]: tag };
   }
 
-  const is = {} as Predicates<any>
+  const is = {} as Predicates<any>;
   for (const tag in record) {
-    is[tag] = ((variant: any) => variant[tagProp] === tag) as any
+    is[tag] = ((variant: any) => variant[tagProp] === tag) as any;
   }
 
-  const as = {} as Casts<Record, any>
+  const as = {} as Casts<Record, any>;
   for (const expectedTag in record) {
-    as[expectedTag] = match(
-      {
-        [expectedTag]: (x: any) => x,
-        default: (val: any) => {
-          throw new Error(`Attempted to cast ${val[tagProp]} as ${expectedTag}`)
-        }
-      }
-    )
+    as[expectedTag] = match({
+      [expectedTag]: (x: any) => x,
+      default: (val: any) => {
+        throw new Error(`Attempted to cast ${val[tagProp]} as ${expectedTag}`);
+      },
+    });
   }
 
   function match(cases: any): (variant: any) => any {
     return (variant: any) => {
-      const k = variant[tagProp]
+      const k = variant[tagProp];
       const handler = cases[k];
       return handler !== undefined
         ? handler(valProp ? variant[valProp] : variant)
-        : cases.default(variant)
-    }
+        : cases.default(variant);
+    };
   }
 
-  return Object.assign({
-    is,
-    as,
-    match,
-    _Record: record,
-  }, creators)
+  return Object.assign(
+    {
+      is,
+      as,
+      match,
+      _Record: record,
+    },
+    creators,
+  );
 }
 
 /**
@@ -119,6 +122,6 @@ export function unionize<Record>(record: Record, tagProp = 'tag', valProp?: stri
  * type `T` for any `T`, but it's really just returning `undefined`. This white lie
  * allows convenient expression of the value types in the record you pass to `unionize`.
  */
-export const ofType = <T>() => undefined as any as T
+export const ofType = <T>() => (undefined as any) as T;
 
-export default unionize
+export default unionize;
