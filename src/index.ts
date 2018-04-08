fd4cab366ef63f1164ce6d778bcb97c6e2ae1c4b
Simon <twop.sk@gmail.com>
2018-04-07 18:26:58 -0700

    simlified currying

--- src/index.ts
+++ src/index.ts
@@ -95,7 +95,8 @@ export function unionize<Record>(record: Record, tagProp = 'tag', valProp?: stri
       : cases.default(variant);
   }
 
-  const match: ReverseCurriedFunc<any, any, any> = reverseCurry(evalMatch);
+  const match = (first: any, second?: any) =>
+    second ? evalMatch(first, second) : (variant: any) => evalMatch(variant, first);
 
   const as = {} as Casts<Record, any>;
   for (const expectedTag in record) {
@@ -116,25 +117,6 @@ export function unionize<Record>(record: Record, tagProp = 'tag', valProp?: stri
     },
     creators,
   );
-}
-
-type ReverseCurriedFunc<A1, A2, R> = {
-  (a1: A1, a2: A2): R;
-  (a2: A2): (a1: A1) => R;
-};
-
-function reverseCurry<A1, A2, R, F extends (a1: A1, a2: A2) => R>(
-  f: F,
-): ReverseCurriedFunc<A1, A2, R> {
-  const func = function reverseCurried(a1: A1 | A2, a2: A2 | undefined) {
-    if (arguments.length == 1) {
-      return (a: A1) => f(a, <A2>a1);
-    }
-
-    return f(<A1>a1, <A2>a2);
-  };
-
-  return (func as any) as ReverseCurriedFunc<A1, A2, R>;
 }
 
 /**
